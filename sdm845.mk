b0ecab19713c7cd3734fd3fdc542e2218914cc57
kubersharma001 <kubersharma001@gmail.com>
2019-09-09 11:15:01 +0530

    sdm845-common: Enable few packages and boot_jars

--- sdm845.mk
+++ sdm845.mk
@@ -172,11 +172,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
 
 # IFAA manager
-#PRODUCT_PACKAGES += \
-#    org.ifaa.android.manager
+PRODUCT_PACKAGES += \
+    org.ifaa.android.manager
 
-#PRODUCT_BOOT_JARS += \
-#    org.ifaa.android.manager
+PRODUCT_BOOT_JARS += \
+    org.ifaa.android.manager
 
 # IMS
 PRODUCT_PACKAGES += \
@@ -278,8 +278,8 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
 # Telephony
-#PRODUCT_PACKAGES += \
-#    telephony-ext
+PRODUCT_PACKAGES += \
+    telephony-ext
 
 #PRODUCT_BOOT_JARS += \
 #    telephony-ext
