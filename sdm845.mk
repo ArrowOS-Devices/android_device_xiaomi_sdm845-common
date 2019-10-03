dfa672ac9313b2bcc5d94f1c048158ba170b00ac
Santhosh Behara <santhoshbehara@codeaurora.org>
2019-10-03 18:26:59 +0530

    sdm845-common: media: Add and publish Codec2 HAL service
    *Add 2 codec2 relates libs to PRODUCT_PACKAGES in order to copy
    them to /vendor/lib/
    *Add seccomp policies for codec2 services
    
    Change-Id: I83af4f6b80886ca18aa059285e0b94403c027e29

--- sdm845.mk
+++ sdm845.mk
@@ -123,6 +123,12 @@ PRODUCT_PACKAGES += \
     Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
 
+# Codec2
+PRODUCT_PACKAGES += \
+    libcodec2_vndk.vendor \
+    libcodec2_hidl@1.0.vendor
+
+
 # Codec2 modules
 PRODUCT_PACKAGES += \
     com.android.media.swcodec \
@@ -290,6 +296,7 @@ PRODUCT_PACKAGES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 
 # Sensors
