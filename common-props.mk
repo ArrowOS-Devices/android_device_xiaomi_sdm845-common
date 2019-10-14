3530e223a4ecf40d616033277d761739cd2b05e3
palaych <palaychm@yandex.ru>
2019-10-14 10:26:02 +0000

    Revert "sdm845-common: [DO NOT MERGE] Set few ril/ims/wfc props"
    This reverts commit 6447fb100704fc4d6284b8379177b8421d2f6f0e.

--- common-props.mk
+++ common-props.mk
@@ -152,15 +152,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.rcs.supported=0 \
-    persist.vendor.ims.disableUserAgent=0 \
-    persist.vendor.radio.force_ltd_sys_ind=1 \
-    persist.vendor.radio.voice_on_lte=1 \
-    persist.vendor.radio.calls.on.ims=1 \
-    persist.dbg.ims_volte_enable=1 \
-    persist.dbg.wfc_avail_ovr=1 \
-    persist.radio.calls.on.ims=1 \
-    persist.dbg.wfc_avail_ovr=1 \
-    persist.dbg.ims_volte_enable=1
+    persist.vendor.ims.disableUserAgent=0
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
