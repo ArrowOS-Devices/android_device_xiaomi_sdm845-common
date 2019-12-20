7c23b4410182d68d6eb8684d6e254808b2f3cc4b
kubersharma001 <kubersharma001@gmail.com>
2019-12-20 12:34:16 +0100

    Revert "sdm845-common: Turn on GPU protected composition."
    This reverts commit 9103242c5d470a12a260e6d2c6e2f1afb5498315.

--- common-props.mk
+++ common-props.mk
@@ -180,7 +180,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=2 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
