c3501b13777840b12ffc2c48327f269af6556d83
palaych <palaychm@yandex.ru>
2020-01-28 14:34:47 +0300

    Revert "sdm845-common: camera: Make shim for libui"
    We already have it in device tree
    
    This reverts commit 30efe4e2afd6921db14a726b421cdd3e7efa2807.

--- sdm845.mk
+++ sdm845.mk
@@ -102,7 +102,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     Snap \
-    libui_shim \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Common init scripts
