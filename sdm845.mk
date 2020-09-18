e7256199e46d72be1210f0aeff4220324f8eed06
LuK1337 <priv.luk@gmail.com>
2020-09-18 13:12:09 +0200

    sdm845-common: Drop android.hardware.thermal HAL
    android.hardware.thermal@1.0::IThermal/default is deprecated in compatibility matrix at FCM Version 4; it should not be served.
    
    Change-Id: Icc3c672a0bf6b9e4ee76ccf91e6cdca5fb778fde

--- sdm845.mk
+++ sdm845.mk
@@ -306,12 +306,6 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sdm845
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
