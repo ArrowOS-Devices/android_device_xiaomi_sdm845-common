26f2fa21521e4f98deda9ee2b83f4645fd6fe754
palaych <palaychm@yandex.ru>
2020-09-24 17:17:00 +0000

    sdm845-common: Drop HotwordEnrollment
    Q blobs don't work on R. Just remove this.
    
    Change-Id: I0526edc5977d48afd0466b37bb4e1f5ac844f3b1

--- sdm845.mk
+++ sdm845.mk
@@ -185,10 +185,6 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhidltransport.vendor
 
-# HotwordEnrollement app permissions
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
-
 # IFAA manager
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
