d00f3e3318c415cf49782b32cb6dde08d51e053d
Pranav Vashi <neobuddy89@gmail.com>
2020-11-04 15:52:37 +0100

    sdm845-common: parts: Add file utils
    Change-Id: Id4840c104de7e828aafec754a8ac5b80769fdff9

--- parts/src/org/lineageos/settings/thermal/ThermalUtils.java
+++ parts/src/org/lineageos/settings/thermal/ThermalUtils.java
@@ -19,7 +19,7 @@ package org.lineageos.settings.thermal;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.FileUtils;
+import org.lineageos.settings.utils.FileUtils;
 import android.os.UserHandle;
 import android.util.Log;
 
@@ -181,11 +181,6 @@ public final class ThermalUtils {
                 state = THERMAL_STATE_STREAMING;
             }
         }
-
-        try {
-            FileUtils.stringToFile(THERMAL_SCONFIG, state);
-        } catch (IOException e) {
-            Log.e(TAG, "Failed to write to " + THERMAL_SCONFIG, e);
-        }
+            FileUtils.writeLine(THERMAL_SCONFIG, state);
     }
 }
