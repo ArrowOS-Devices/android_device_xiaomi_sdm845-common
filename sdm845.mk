c7f3f88e57f1e40b59a69d28b88d6858bcd1d835
Alex Damaratski <alexeydomoratsky1@gmail.com>
2021-03-19 09:29:19 +0300

    sdm845-common: Build Mock Powerstats HAL
    * E BatteryStatsService: Unable to load Power Hal or power.stats HAL
    
    Signed-off-by: Alex Damaratski <alexeydomoratsky1@gmail.com>
    Change-Id: Id9abdc668108e3a864056778acca1d2889316fe0

--- sdm845.mk
+++ sdm845.mk
@@ -275,7 +275,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-libperfmgr
+    android.hardware.power-service.xiaomi-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
